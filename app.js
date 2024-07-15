7a7792ab3c2fe467ecb500938ab1d62c4d2554f3
SAYUBA <sho.yuba48@gmail.com>
2024-07-15 17:03:01 +0900

    Update CSS with bootstrap

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const path = require("path");
 const mongoose = require("mongoose");
+const ejsMate = require('ejs-mate');
 const methodOverride = require("method-override");
 const Campground = require("./models/campground");
 
@@ -20,6 +21,7 @@ db.once("open", () => {
 });
 
 const app = express();
+app.engine('ejs', ejsMate)
 app.set("view engine", "ejs");
 app.set("veiws", path.join(__dirname, "views"));
 app.use(express.urlencoded({ extended: true }));
@@ -27,7 +29,7 @@ app.use(methodOverride("_method"));
 
 // --- Routing ---
 app.get("/", (req, res) => {
-	res.send("hello");
+	res.render("home");
 });
 
 app.get("/campgrounds", async (req, res) => {
--- seeds/index.js
+++ seeds/index.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const cities = require("./cities");
 const { places, descriptors } = require("./seedHelpers");
 const Campground = require("../models/campground");
-
+// image: `https://picsum.photos/400?random=${Math.random()}`
 mongoose
 	.connect("mongodb://localhost:27017/yelp-camp")
 	.then(() => {
@@ -24,16 +24,20 @@ const sample = (array) => {
 
 const seedDB = async () => {
 	await Campground.deleteMany({});
+	const price = Math.floor(Math.random() * 20) + 20;
 	for (let i = 0; i < 50; i++) {
 		const random1000 = Math.floor(Math.random() * 1000);
 		const camp = new Campground({
 			location: `${cities[random1000].city}, ${cities[random1000].state}`,
 			title: `${sample(descriptors)} ${sample(places)}`,
+			image: `https://picsum.photos/400?random=${Math.random()}`,
+			description: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur tempora numquam quia accusamus odio molestiae, sed repellat qui quam natus distinctio explicabo! Nostrum eveniet animi est ullam sit quibusdam voluptatum?`,
+			price: price
 		});
 		await camp.save();
 	}
 };
 
-seedDB().then(()=>{
+seedDB().then(() => {
 	mongoose.connection.close();
 });
