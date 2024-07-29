1a590b679b429719de8773acd5fd794a898904f8
SAYUBA <sho.yuba48@gmail.com>
2024-07-29 22:05:49 +0900

    refactor routing in fancy way

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -6,13 +6,19 @@ const catchAsync = require('../utils/catchAsync');
 const {isLoggedIn, isAuthor, validateCampground} = require('../middleware');
 
 // routing
-router.get('/', catchAsync(campgrounds.index));
-router.get('/new', isLoggedIn, campgrounds.renderNewForm);
-router.post('/', isLoggedIn, validateCampground, catchAsync(campgrounds.createCampground));
+router
+	.route('/')
+	.get(catchAsync(campgrounds.index))
+	.post(isLoggedIn, validateCampground, catchAsync(campgrounds.createCampground));
 
-router.get('/:id', catchAsync(campgrounds.showCampground));
+router.get('/new', isLoggedIn, campgrounds.renderNewForm);
+
+router
+	.route('/:id')
+	.get(catchAsync(campgrounds.showCampground))
+	.put(isLoggedIn, isAuthor, validateCampground, catchAsync(campgrounds.updateCampground))
+	.delete(isLoggedIn, isAuthor, catchAsync(campgrounds.deleteCampground));
+
 router.get('/:id/edit', isLoggedIn, isAuthor, catchAsync(campgrounds.renderEditForm));
-router.put('/:id', isLoggedIn, isAuthor, validateCampground, catchAsync(campgrounds.updateCampground));
-router.delete('/:id', isLoggedIn, isAuthor, catchAsync(campgrounds.deleteCampground));
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -5,18 +5,12 @@ const catchAsync = require('../utils/catchAsync');
 const {storeReturnTo} = require('../middleware');
 const users = require('../controllers/users');
 
-router.get('/register', users.renderRegister);
+router.route('/register').get(users.renderRegister).post(catchAsync(users.register));
 
-router.post('/register', catchAsync(users.register));
-
-router.get('/login', users.renderLogin);
-
-router.post(
-	'/login',
-	storeReturnTo,
-	passport.authenticate('local', {failureFlash: true, failureRedirect: '/login'}),
-	users.login
-);
+router
+	.route('/login')
+	.get(users.renderLogin)
+	.post(storeReturnTo, passport.authenticate('local', {failureFlash: true, failureRedirect: '/login'}), users.login);
 
 router.get('/logout', users.logout);
 
