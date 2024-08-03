32e205b3870af9413b23600e95ad2f1feccbd47d
SAYUBA <sho.yuba48@gmail.com>
2024-08-03 18:01:04 +0900

    Proto: mapbox2

--- seeds/index.js
+++ seeds/index.js
@@ -25,7 +25,7 @@ const sample = array => {
 const seedDB = async () => {
 	await Campground.deleteMany({});
 	const price = Math.floor(Math.random() * 20) + 20;
-	for (let i = 0; i < 50; i++) {
+	for (let i = 0; i < 300; i++) {
 		const random1000 = Math.floor(Math.random() * 1000);
 		const camp = new Campground({
 			// (user, <-same)
@@ -36,7 +36,10 @@ const seedDB = async () => {
 			price: price,
 			geometry: {
 				type: 'Point',
-				coordinates: [-133.1331, 470202]
+				coordinates: [
+                    cities[random1000].longitude,
+                    cities[random1000].latitude,
+                ]
 			},
 			images: [
 				{
