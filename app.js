4926d486d9e66abf7ed4a62f6b60555edab0ac37
SAYUBA <sho.yuba48@gmail.com>
2024-08-09 23:08:17 +0900

    update dbUrl

--- app.js
+++ app.js
@@ -23,8 +23,8 @@ const usersRoutes = require('./routes/users');
 
 const MongoStore = require('connect-mongo');
 
-// const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/yelp-camp';
-const dbUrl = 'mongodb://localhost:27017/yelp-camp';
+const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/yelp-camp';
+// const dbUrl = 'mongodb://localhost:27017/yelp-camp';
 mongoose
     .connect(dbUrl)
 	.then(() => {
